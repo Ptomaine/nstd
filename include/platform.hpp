a7868648ceea6fcfa7d638f6c0552d5aac09e437
Arlen Keshabyan <arlen.albert@gmail.com>
2018-12-14 15:10:58 -0800

    endianness function was added

--- include/platform.hpp
+++ include/platform.hpp
@@ -426,4 +426,36 @@ inline uint32_t change_endianness(uint32_t value)
     return result;
 }
 
+enum endianness_type : uint8_t
+{
+    ENDIAN_UNKNOWN,
+    ENDIAN_BIG,
+    ENDIAN_LITTLE,
+    ENDIAN_BIG_WORD,   /* Middle-endian, Honeywell 316 style */
+    ENDIAN_LITTLE_WORD /* Middle-endian, PDP-11 style */
+};
+
+inline endianness_type endianness()
+{
+    union
+    {
+        uint32_t value;
+        uint8_t data[sizeof(uint32_t)];
+    } number;
+
+    number.data[0] = 0x00;
+    number.data[1] = 0x01;
+    number.data[2] = 0x02;
+    number.data[3] = 0x03;
+
+    switch (number.value)
+    {
+    case UINT32_C(0x00010203): return ENDIAN_BIG;
+    case UINT32_C(0x03020100): return ENDIAN_LITTLE;
+    case UINT32_C(0x02030001): return ENDIAN_BIG_WORD;
+    case UINT32_C(0x01000302): return ENDIAN_LITTLE_WORD;
+    default:                   return ENDIAN_UNKNOWN;
+    }
+}
+
 }
