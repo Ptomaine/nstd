dbc69cf488cd5f3faa842ef5f6659391a919704c
Arlen Keshabyan <arlen.albert@gmail.com>
2019-03-05 11:53:01 -0800

    minor optimization

--- examples/http_resource_manager_example.cpp
+++ examples/http_resource_manager_example.cpp
@@ -121,7 +121,7 @@ int main(int argc, char *argv[])
             if (std::size(media_type) > 0 && media_type[0] == '.') media_type = media_type.substr(1);
 
             if (const auto &[type_exists, it] = media_types::find(media_type);
-                fs::exists(full_path) && fs::is_regular_file(full_path) && type_exists)
+                type_exists && fs::exists(full_path) && fs::is_regular_file(full_path))
             {
                 typename http_resource_manager::response resp { S::OK };
                 auto data { nstd::utilities::read_file_content(full_path) };
