b3eedcf472fe4d6cde4713ee2f00bceabc5380ce
Arlen Keshabyan <arkeshab@microsoft.com>
2020-02-21 14:00:46 -0800

    uint8_t type was replace with more appropriate the char8_t one

--- include/utf8.hpp
+++ include/utf8.hpp
@@ -37,9 +37,9 @@ constexpr const uint32_t surrogate_offset    = 0x10000u - (lead_surrogate_min <<
 constexpr const uint32_t code_point_max      = 0x0010ffffu;
 
 template<typename octet_type>
-inline uint8_t mask8(octet_type oc)
+inline char8_t mask8(octet_type oc)
 {
-    return static_cast<uint8_t>(0xff & oc);
+    return static_cast<char8_t>(0xff & oc);
 }
 
 template<typename u16_type>
@@ -81,7 +81,7 @@ inline bool is_code_point_valid(u32 cp)
 template <typename octet_iterator>
 inline auto sequence_length(octet_iterator lead_it) -> typename std::iterator_traits<octet_iterator>::difference_type
 {
-    uint8_t lead = utf8::internal::mask8(*lead_it);
+    char8_t lead = utf8::internal::mask8(*lead_it);
 
          if ( lead < 0x80) return 1;
     else if ((lead >> 5) == 0x6) return 2;
@@ -275,7 +275,7 @@ inline bool is_valid(octet_iterator start, octet_iterator end)
     return (utf8::find_invalid(start, end) == end);
 }
 
-constexpr static inline const uint8_t bom[] = { 0xef, 0xbb, 0xbf };
+constexpr static inline const char8_t bom[] = { 0xef, 0xbb, 0xbf };
 
 template <typename octet_iterator>
 inline bool starts_with_bom(octet_iterator it, octet_iterator end)
@@ -300,11 +300,11 @@ public:
 
 class invalid_utf8 : public exception
 {
-    uint8_t u8;
+    char8_t u8;
 public:
-    invalid_utf8 (uint8_t u) : u8(u) {}
+    invalid_utf8 (char8_t u) : u8(u) {}
     virtual const char* what() const throw() { return "Invalid UTF-8"; }
-    uint8_t utf8_octet() const {return u8;}
+    char8_t utf8_octet() const {return u8;}
 };
 
 class invalid_utf16 : public exception
@@ -327,24 +327,24 @@ octet_iterator append(uint32_t cp, octet_iterator result)
 {
     if (!utf8::internal::is_code_point_valid(cp)) throw invalid_code_point(cp);
 
-    if (cp < 0x80) *(result++) = static_cast<uint8_t>(cp);
+    if (cp < 0x80) *(result++) = static_cast<char8_t>(cp);
     else if (cp < 0x800)
     {
-        *(result++) = static_cast<uint8_t>((cp >> 6)            | 0xc0);
-        *(result++) = static_cast<uint8_t>((cp & 0x3f)          | 0x80);
+        *(result++) = static_cast<char8_t>((cp >> 6)            | 0xc0);
+        *(result++) = static_cast<char8_t>((cp & 0x3f)          | 0x80);
     }
     else if (cp < 0x10000)
     {
-        *(result++) = static_cast<uint8_t>((cp >> 12)           | 0xe0);
-        *(result++) = static_cast<uint8_t>(((cp >> 6) & 0x3f)   | 0x80);
-        *(result++) = static_cast<uint8_t>((cp & 0x3f)          | 0x80);
+        *(result++) = static_cast<char8_t>((cp >> 12)           | 0xe0);
+        *(result++) = static_cast<char8_t>(((cp >> 6) & 0x3f)   | 0x80);
+        *(result++) = static_cast<char8_t>((cp & 0x3f)          | 0x80);
     }
     else
     {
-        *(result++) = static_cast<uint8_t>((cp >> 18)           | 0xf0);
-        *(result++) = static_cast<uint8_t>(((cp >> 12) & 0x3f)  | 0x80);
-        *(result++) = static_cast<uint8_t>(((cp >> 6) & 0x3f)   | 0x80);
-        *(result++) = static_cast<uint8_t>((cp & 0x3f)          | 0x80);
+        *(result++) = static_cast<char8_t>((cp >> 18)           | 0xf0);
+        *(result++) = static_cast<char8_t>(((cp >> 12) & 0x3f)  | 0x80);
+        *(result++) = static_cast<char8_t>(((cp >> 6) & 0x3f)   | 0x80);
+        *(result++) = static_cast<char8_t>((cp & 0x3f)          | 0x80);
     }
     return result;
 }
@@ -578,24 +578,24 @@ namespace unchecked
 template <typename octet_iterator>
 octet_iterator append(uint32_t cp, octet_iterator result)
 {
-    if (cp < 0x80) *(result++) = static_cast<uint8_t>(cp);  
+    if (cp < 0x80) *(result++) = static_cast<char8_t>(cp);  
     else if (cp < 0x800)
     {
-        *(result++) = static_cast<uint8_t>((cp >> 6)          | 0xc0);
-        *(result++) = static_cast<uint8_t>((cp & 0x3f)        | 0x80);
+        *(result++) = static_cast<char8_t>((cp >> 6)          | 0xc0);
+        *(result++) = static_cast<char8_t>((cp & 0x3f)        | 0x80);
     }
     else if (cp < 0x10000)
     {
-        *(result++) = static_cast<uint8_t>((cp >> 12)         | 0xe0);
-        *(result++) = static_cast<uint8_t>(((cp >> 6) & 0x3f) | 0x80);
-        *(result++) = static_cast<uint8_t>((cp & 0x3f)        | 0x80);
+        *(result++) = static_cast<char8_t>((cp >> 12)         | 0xe0);
+        *(result++) = static_cast<char8_t>(((cp >> 6) & 0x3f) | 0x80);
+        *(result++) = static_cast<char8_t>((cp & 0x3f)        | 0x80);
     }
     else
     {                                // four octets
-        *(result++) = static_cast<uint8_t>((cp >> 18)         | 0xf0);
-        *(result++) = static_cast<uint8_t>(((cp >> 12) & 0x3f)| 0x80);
-        *(result++) = static_cast<uint8_t>(((cp >> 6) & 0x3f) | 0x80);
-        *(result++) = static_cast<uint8_t>((cp & 0x3f)        | 0x80);
+        *(result++) = static_cast<char8_t>((cp >> 18)         | 0xf0);
+        *(result++) = static_cast<char8_t>(((cp >> 12) & 0x3f)| 0x80);
+        *(result++) = static_cast<char8_t>(((cp >> 6) & 0x3f) | 0x80);
+        *(result++) = static_cast<char8_t>((cp & 0x3f)        | 0x80);
     }
 
     return result;
